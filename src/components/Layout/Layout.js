c88317b44cd698db59b8bb0846e2b17a101d7da7
Michael <michaeldelfante@gmail.com>
2021-02-21 15:56:47 -0800

    created burger, ingredients, and build controls

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <React.Fragment>
         <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </React.Fragment>
