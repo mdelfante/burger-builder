c72c6ec3381d474de87d6840b1884eb152b8364a
Michael <michaeldelfante@gmail.com>
2021-02-22 18:27:19 -0800

    finished building the burger part of app

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return <li key={igKey}>
+                        <span 
+                            style={{textTransform: 'capitalize'}}>{igKey}
+                        </span>: {props.ingredients[igKey]}
+                    </li>;
+        });
+
+    return (
+        <React.Fragment>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: {props.price}</strong></p>
+            <p>Continue to checkout?</p>
+            <Button 
+                clicked={props.cancel}
+                buttonType="Danger">CANCEL</Button>
+            <Button 
+                clicked={props.continue}
+                buttonType="Success">CONTINUE</Button>
+        </React.Fragment>
+    );
+};
+
+export default orderSummary;
