04a309bfa1e7ef93a7cfe4f83ad786138fb12b96
Michael <michaeldelfante@gmail.com>
2021-02-25 17:37:58 -0800

    made calls to server, added loading, and error handling

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <React.Fragment>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </React.Fragment>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.show !== nextProps.show || this.props.children !== nextProps.children;
+    }
 
-export default modal;
+    render() {
+        return (
+            <React.Fragment>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Modal;
